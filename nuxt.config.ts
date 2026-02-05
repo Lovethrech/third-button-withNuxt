dcb4b1cfb163e9d1a6beaffcbb4fcef030761174
Lovethrech <rachealdolapo45@gmail.com>
2026-02-05 15:30:34 +0100

    Initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,30 @@ export default defineNuxtConfig({
           name:'apple-mobile-web-app-capable',
           content:'yes'
         }
+      ],
+      link:[
+        {
+          rel:'icon',
+          href: './src/assets/logo.png'
+        },
+        {
+          rel:'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/remixicon@4.8.0/fonts/remixicon.css'
+        },
+        {
+          rel:'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel:'preconnect',
+          href: 'https://fonts.gstatic.com',
+        },
+        {
+          rel:'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Cormorant+Unicase:wght@300;400;500;600;700&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap'
+        }
       ]
-    }
+    },
   }
+
 })
