596ae26c785d41e8609d95b3b30ba76ed24dbab4
Lovethrech <rachealdolapo45@gmail.com>
2026-02-05 15:14:57 +0100

    Initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app:{
     head:{
-      title:'ThirdButton IT Solution'
+      title:'ThirdButton IT Solution',
+      meta:[
+        {
+          name:'viewport',
+          content: 'width=device-width,initial-scale=1'
+        },
+        {
+          name:'description',
+          content:''
+        }
+      ]
     }
   }
 })
