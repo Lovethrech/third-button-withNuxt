d5d07c855e6f75de2dc0abfdb3953dfe6143f8ae
Lovethrech <rachealdolapo45@gmail.com>
2026-02-13 21:47:57 +0100

    Setting up the page and contents

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,44 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app:{
     head:{
-      meta:[
-        {
-          name:'viewport',
-          content: 'width=device-width,initial-scale=1'
-        },
-        {
-          name:'application-name',
-          content:'ThirdButton'
-        },
-        {
-          name:'author',
-          content:'Olaonipekun Dolapo Rachael'
-        },
-        {
-          name:'description',
-          content:'ThirdButton IT Consulting Company'
-        },
-        {
-          name:'keywords',
-          content:'ThirdEyeButton, thirdeyebutton, thirdEyebutton, thirdEyeButton,ThirdEyeButton IT Comapany, ThirdEyeButton IT Consultant, ThirdEyeButton IT Consulting Company, ThirdEyeButton It Consulting company'
-        },
-        {
-          name:'robots',
-          content:'notranslate'
-        },
-        {
-          name:'robots',
-          content:'noimageindex'
-        },
-        {
-          name:'mobile-web-app-capable',
-          content:'yes'
-        },
-        {
-          name:'apple-mobile-web-app-capable',
-          content:'yes'
-        }
-      ],
+      
       link:[
         {
           rel:'icon',
