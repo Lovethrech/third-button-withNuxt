b64238fed5054d518c4adf9ef5c61e1cd845c100
Lovethrech <rachealdolapo45@gmail.com>
2026-02-13 21:48:31 +0100

    Setting up the page and contents

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  app:{
-    head:{
-      
-      link:[
-        {
-          rel:'icon',
-          href: './src/assets/logo.png'
-        },
-        {
-          rel:'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/remixicon@4.8.0/fonts/remixicon.css'
-        },
-        {
-          rel:'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel:'preconnect',
-          href: 'https://fonts.gstatic.com',
-        },
-        {
-          rel:'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Cormorant+Unicase:wght@300;400;500;600;700&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap'
-        }
-      ]
-    }
-  }
+  devtools: { enabled: true }
 })
