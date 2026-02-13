80baa11f265a27560f5ea4897fc29c08a0e8a9da
Lovethrech <rachealdolapo45@gmail.com>
2026-02-13 23:35:15 +0100

    Starting the project...

---
+++ app/components/main/header/logo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div 
+        id="header-logo-ctn"
+    >
+        <RouterLink 
+            to="/"
+        >
+            <img 
+                src="/src/assets/logo.png" 
+                alt="ThirdEyeButton Logo"
+            /> 
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped>
+#header-logo-ctn{
+    width:8vh;
+}
+#header-logo-ctn img{
+    width: 100%;
+}
+</style>
