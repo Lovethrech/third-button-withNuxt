6b66ba20f84c1f9fc17a28759261008ad8596557
Lovethrech <rachealdolapo45@gmail.com>
2026-02-14 00:19:21 +0100

    Working on Header

---
+++ app/components/main/header/menuCtn.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import HeaderMenuDetails from "@/data/header-menu-details.json";
+import HeaderMenu from './HeaderMenu.vue';
+</script>
+
+<template>
+    <div 
+        class="header-menu-ctn-main-ctn">
+        <div 
+            v-for="HeaderMenuDetail in HeaderMenuDetails"  
+            class="header-menu-ctn-mini-ctn" 
+            :key="HeaderMenuDetail.id"
+        >
+            <HeaderMenu 
+                :name="HeaderMenuDetail.name" 
+                :link="HeaderMenuDetail.link"
+            >
+            </HeaderMenu>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header-menu-ctn-main-ctn{
+    display:flex;
+    flex-direction: row;
+    gap: 1.4vw;
+    margin: auto 0;
+}
+
+@media screen and (max-width:990px) {
+    .header-menu-ctn-main-ctn{
+        flex-direction: column;
+        margin:0;
+        width: 100%;
+        text-align:center
+    }
+}
+
+@media screen and (width: 1024px) and (min-height:1360px) {
+    .header-menu-ctn-main-ctn{
+        flex-direction: column;
+        margin:0;
+        width: 100%;
+        text-align:center
+    }
+}
+</style>
---
+++ app/components/main/header/navCtn.vue
@@ -0,0 +1,44 @@
+<template>
+    <div 
+        class="header-nav-ctn"
+    >
+        
+        <!-- <HeaderMenuCtn></HeaderMenuCtn>
+        <HeaderClientBtn></HeaderClientBtn> -->
+    </div>
+</template>
+
+<style scoped>
+.header-nav-ctn{
+    margin: auto 0;
+    display: flex;
+    flex-direction: row;
+    gap:2vw;
+}
+@media screen and (max-width:990px){
+    .header-nav-ctn{
+        position: absolute;
+        top: 7.8vh;
+        right:0;
+        flex-direction: column;
+        gap: 4vh;
+        width: 100%;
+        height: 90vh;
+        background: linear-gradient(to bottom, #cae0ff, #ffffff);
+    }
+}
+
+@media screen and (width: 1024px) and (min-height:1360px) {
+    .header-nav-ctn{
+        position: absolute;
+        top: 7.8vh;
+        right:0;
+        flex-direction: column;
+        gap: 4vh;
+        width: 100%;
+        height: 90vh;
+        background: linear-gradient(to bottom, #cae0ff, #ffffff);
+        display:none;
+    }
+}
+</style>
