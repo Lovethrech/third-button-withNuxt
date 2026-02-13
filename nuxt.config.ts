e179d151d3cd36dd10f513d345a3ff695f9dc3cc
Lovethrech <rachealdolapo45@gmail.com>
2026-02-13 23:11:17 +0100

    Setting up the page and contents

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  srcDir: './app'
+  dir: {
+    pages: 'app/pages',
+    layouts: 'app/layouts',
+    app: 'app'
+  }
 })
